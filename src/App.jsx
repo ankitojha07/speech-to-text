357ad0906f76f3f33d1341b4565c518ea0d68593
Ankit Ojha <ankitojha1409@gmail.com>
2023-04-09 22:16:37 +0530

    working on the home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <>
+      <div className="container">
+        <h2>Speech to text convertor</h2>
+        <br />
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          A React hook that convert speech from microphone to text and make it
+          available for you to use!
         </p>
+        <div className="main-content">
+          <div className="btn-style">
+            <button>Copy</button>
+            <button>Start listening</button>
+            <button>Stop listening</button>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   );
-}
+};
 
 export default App;
